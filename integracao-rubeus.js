e0bc5690f95734fe0af651cfb2e39da01d48c8ee
Jonatas Macedo <jonatasmacedo@rubeus.com.br>
2019-09-27 14:25:48 -0300

    adição tag beta no titulo

--- integracao-rubeus.js
+++ integracao-rubeus.js
@@ -1,4 +1,4 @@
-function enviarContatoRubeus() {
+function sendContactRubeus() {
     const data = {
         nome: document.getElementById("input_nome").value,
         emailPrincipal: document.getElementById("input_email").value,
@@ -61,4 +61,32 @@ function unsetScriptTracking() {
     script.parentNode.removeChild(script);
     script.removeAttribute('src');
     document.head.appendChild(script);
+}
+
+function validateNome() {
+    const nomeField = document.getElementById("input_nome");
+    const regexNome = /^(?![ ])(?!.*[ ]{2})((?:e|da|do|das|dos|de|d'|D'|la|las|el|los)\s*?|(?:[A-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð'][^\s]*\s*?)(?!.*[ ]$))+$/;
+
+    changeBorderField(regexNome, nomeField);
+}
+
+function validateEmail() {
+    const regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    const emailField = document.getElementById("input_email");
+    
+    changeBorderField(regexEmail, emailField);
+}
+
+function validateTelefone() {
+    const regexTelefone = /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/;
+    const telefoneField = document.getElementById("input_telefone");
+    
+    changeBorderField(regexTelefone, telefoneField);
+}
+
+function changeBorderField(regex, field) {
+    field.className = 'form-control border border-danger'
+    if (!!field.value.match(regex)) {
+        field.className = 'form-control border border-success'
+    }
 }
